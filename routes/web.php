26548ae20342b19d20d1bf364c89cea321f26cbc
Cody Mac <nicoaudy1994@gmail.com>
2018-11-11 08:20:23 +0700

    initial commit p-2

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,5 @@
 <?php
 
-Route::get('/', 'HelloController@index');
-
-Route::get('hello/{name}', function($name){
-	return 'Hello ' . $name;
+Route::get('/', function(){
+	return view('layouts.main');
 });
-
-Route::get('hello/controller/{name}', 'HelloController@show');
-Route::get('hello/name/{name}/umur/{age?}', 'HelloController@withCondition');
-
-Route::get('first', function(){
-	return 'ini route pertama';	
-})->name('first.route');
-
-Route::get('kedua', function(){
-	return redirect()->route('first.route');
-});
-
-// Route::get('hello/name/{name}/umur/{age?}', function($name, $age = null) {
-// 	if($age == null) {
-// 		return 'Umur anda kosong. Isi dong!';
-// 	}
-// 	return "Hello nama saya {$name} umur saya {$age}";
-// });
